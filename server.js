c6ccde8f0963028f7642bc4638b9249362330d86
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-06-28 14:21:39 -0400

    add sql query

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Xela3221',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //default response for any other request (not found)
 app.use((req, res) => {
